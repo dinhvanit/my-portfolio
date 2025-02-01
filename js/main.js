aad90af875cc8c5f82c861ecaa9b6c228c232564
dinhvanit <nguyendinhvanefto@gmail.com>
2025-02-01 13:46:28 +0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Cảm ơn bạn đã liên hệ! Tôi sẽ phản hồi sớm nhất có thể.");
+  });
